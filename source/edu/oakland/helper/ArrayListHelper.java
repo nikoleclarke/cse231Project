dafa5d56ae4096f8377c9bd4afdd667b73b41569
robinonsay <raomax@gmail.com>
2016-03-31 10:06:26 -0400

    On branch search  Changes to be committed: 	modified:   source/edu/oakland/helper/ArrayListHelper.java Added method

--- source/edu/oakland/helper/ArrayListHelper.java
+++ source/edu/oakland/helper/ArrayListHelper.java
@@ -5,4 +5,13 @@ public class ArrayListHelper{
 
   private ArrayList arrayList;
 
+  public ArrayListHelper(ArrayList<Integer> arrayList){
+    this.arrayList = arrayList;
+  }
+
+
+  public int find(int number){
+    
+
+  }
 }
