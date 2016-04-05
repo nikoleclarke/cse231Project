d7052bbdd265c4175e5939b04985fd2ec40edb6d
Dillon <Smedleydillon@yahoo.com>
2016-04-05 11:44:19 -0400

    Hard coded test array
    Added hard coded test array so we know what the result should be. Still
    have to call functions to get the results.

--- source/edu/oakland/test/ArrayListTest.java
+++ source/edu/oakland/test/ArrayListTest.java
@@ -7,6 +7,7 @@ import edu.oakland.helper.*;
 public class ArrayListTest extends TestCase{
 	
 	private int[] intArray;
+	private int[] finalArray;
 	private ArrayList<Integer> arrayList;
 	private ArrayList<Integer> sortedArrayList;
 	
@@ -14,23 +15,20 @@ public class ArrayListTest extends TestCase{
 		ArrayListGen aListGen = new ArrayListGen();
 		ArrayListHelper aListHelper;
 		
-		intArray = new int[20];
-		
-		for (int i = 0; i < intArray.length; i++) {
-			intArray[i] = i+1;
-		}
+		intArray = {20, 15, 39, 2, 50, 101, 97, 85, 9, 43};
+		finalArray = {9, 15};	
 		
 		aListGen.generateArrayList(intArray);
 		arrayList = aListGen.getArrayList();
 		
 		aListHelper = new ArrayListHelper(arrayList);
 		sortedArrayList = aListHelper.bubbleSort();
-		
-		
 			
 	}
 
 	public void testSort(){
-
+		
+		Assert.assertArrayEquals( finalArray, result ); //result -> sortedArrayList.get... get first two in array
 	}
+
 }
