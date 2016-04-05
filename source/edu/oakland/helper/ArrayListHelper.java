2505f372c7cf483675b1db310679688d9ca15a07
Nikole Clarke <clarkenikole@gmail.com>
2016-04-05 15:41:30 -0400

    Updated file to include Big O Time

--- source/edu/oakland/helper/ArrayListHelper.java
+++ source/edu/oakland/helper/ArrayListHelper.java
@@ -1,79 +1,76 @@
 package edu.oakland.helper;
 import java.util.*;
 
-public class ArrayListHelper{
+public class ArrayListHelper {
 
-	  private ArrayList<Integer> arrayList;
-	  private long startSortTime, endSortTime, totalSortTime;
+	private long startTime;
+	private long endTime;
+	private long totalTime;
 
-  public ArrayListHelper(ArrayList<Integer> arrayList){
-    this.arrayList = arrayList;
-  }
+	private ArrayList<Integer> arrayList;
 
-  public ArrayList<Integer> bubbleSort(){
-  	  
-      /*
-      * i starts at the end of the array As it is decremented all
-      * indexes greater then they are sorted
-      */
-      
-      startSortTime = System.currentTimeMillis();
+	public ArrayListHelper(ArrayList<Integer> arrayList){
+		this.arrayList = arrayList;
+	}
 
-      for(int i = arrayList.size() - 1; i > 1; 1--){
+		public ArrayList<Integer> bubbleSort(){
 
-        /*
-        *The inner loop starts at the beginning of the array and
-        *compares each value next to each other. If the value is greater
-        * then they are swapped
-        */
+		/*
+		* i starts at the end of the array As it is decremented all
+		* indexes greater then they are sorted
+		*/
 
-        for(int j = 0; j < i; j++){
+		for (int i = arrayList.size() - 1; i > 1; i--) {
 
-          //To change sort to Descending change
+		/*
+		*The inner loop starts at the beginning of the array and
+		*compares each value next to each other. If the value is greater
+		* then they are swapped
+		*/
 
-          if(arrayList.get(j) > arrayList.get(j +1)) {
+			for (int j = 0; j < i; j++) {
 
-            swapValues(j, j+1);
-            }
+				//To change sort to Decending change
 
-        }
+				if (arrayList.get(j) > arrayList.get(j +1)) {
+					swapValues(j, j+1);
+				}
 
-      }
-      
-      endSortTime = System.currentTimeMillis();
-      totalSortTime = endSortTime - startSortTime;
-      
-      System.out.println("Time elapsed during Bubble Sort: " + (totalSortTime) + " milliseconds.");
-      
-      return ArrayList;
-  }
+			}
 
-  public void swapValues(int indexOne, int indexTwo){
-    int temp = arrayList.get(indexOne);
-    arrayList.get(indexOne) = arrayList.get(indexTwo);
-    arrayList.get(indexTwo) = temp;
-  }
+		  }
+		  return arrayList;
+	  }
 
-  public int find(int number){
-    return find(number, 0, arrayList.size()-1);
-  }
+public void swapValues(int indexOne, int indexTwo) {
+	int temp = arrayList.get(indexOne);
+	arrayList.set(indexOne, arrayList.get(indexTwo));
+	arrayList.set(indexTwo, temp);
+}
 
-  private int find(int number, int left, int right){
-    int elementIndex = (right-left)/2;
-    elementIndex += left;
-    int element = arrayList.get(elementIndex);
+public int find(int number) {
+	startTime = System.currentTimeMillis();
+	int result = find(number, 0, arrayList.size()-1);
+	endTime = System.currentTimeMillis();
+	totalTime = System.out.println("Finding the odd numbers took " + (endTime - startTime));
+	return result;
+}
 
-    if(number < element){
-      find(number, left, elementIndex);
-    }else if (number > element){
-      find(number, elementIndex, right);
-    }
+public long getTime() {
+	return totalTime;
+}
 
-    return element;
-  }
-  
-  public long getSortTime(){
-  	
-  	  return totalSortTime;
-  }
+private int find(int number, int left, int right) {
+	int elementIndex = (right-left)/2;
+	elementIndex += left;
+	int element = arrayList.get(elementIndex);
+
+	if (number < element) {
+		find(number, left, elementIndex);
+	} else if (number > element){
+		find(number, elementIndex, right);
+	}
+
+return element;
+}
 }
