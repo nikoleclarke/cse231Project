b4152368fb3ffe44a21185c9ee4d797659ccebb9
Dillon <Smedleydillon@yahoo.com>
2016-04-12 10:18:49 -0400

    added more javadoc

--- source/edu/oakland/test/ArrayListTest.java
+++ source/edu/oakland/test/ArrayListTest.java
@@ -14,10 +14,17 @@ import edu.oakland.helper.*;
 
 public class ArrayListTest extends TestCase{
 	
+	/**
+	*finalArray is the int array used to test the results
+	*aListHelper is the instance of the helper used to get the 
+	*/
 	private int[] finalArray;
-	private ArrayList<Integer> sortedArrayList;
 	private ArrayListHelper aListHelper;
 	
+	/**
+	*setUp() method is used to create the test array and also create
+	*the instance of ArrayListHelper use to get the results
+	*/
 	public void setUp(){
 		//Test array
 		int[] intArray = {200, 150, 391, 20, 500, 1011, 971, 850, 90, 430};
@@ -28,6 +35,10 @@ public class ArrayListTest extends TestCase{
 		aListHelper = new ArrayListHelper(arrayList);
 	}
 
+	/**
+	*testSort() method checks the results of aListHelp giving a green or
+	*red bar depending on the result.
+	*/
 	public void testSort(){
 		//assertArrayEquals(finalArray, aListHelper.getValues()); //result -> sortedArrayList.get... get first two in array
 		assertNotNull(aListHelper.getTime());
